ec5859192c5aa671680a573bfcda4f5c4e7cf446
zeuse <zeuserosmardel@hotmail.com.ar>
2024-07-23 15:05:01 -0300

    Details.

--- src/main/java/com/zeustesta/apirest/auth/AuthController.java
+++ src/main/java/com/zeustesta/apirest/auth/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
